d58a26607ee7e4824d50216b9f727ed403f876b6
Parth Bramhecha <117520008+PARTH-BRAMHECHA@users.noreply.github.com>
2025-06-07 15:38:50 +0530

    Update download_utils.py

--- download_utils.py
+++ download_utils.py
@@ -1,14 +1,7 @@
 import gdown
 import os
 
-def download_files():
-    files = {
-        "movies_list.pkl": "1ENIoe5po9YB5Z-JCcQl87fr2l6PeAHMo",   # Replace with actual ID
-        "similarity.pkl": "1ex89v7kguzpKD0X3ieQWgERiMCnUqKiQ",   # Replace with actual ID
-    }
-
-    for filename, file_id in files.items():
-        if not os.path.exists(filename):
-            url = f"https://drive.google.com/uc?id={file_id}"
-            print(f"Downloading {filename}...")
-            gdown.download(url, filename, quiet=False)
+def download_file_from_google_drive(file_id, dest_path):
+    if not os.path.exists(dest_path):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        gdown.download(url, dest_path, quiet=False)
